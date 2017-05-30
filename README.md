77c69433496a5700a56597a3c46a95340eb1f939
Bahadir Yilmaz <bahadir.yilmaz@avivasa.com.tr>
2017-05-30 11:58:36 +0300

    working on default and static methods in interface

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ Java8 Basics and Pragmatic Usage with Examples
 * [Optionals](#optionals)
    
 -----------
+
+### Interface: default and static methods
+
+- default methods
+	- extends the interface without the fear of breaking inheritance chain
+	- multi-type behavioral inheritance is now possible
+	- base class implementation is now not a necessity
+	- the difference between abstract class and interface is lessened
+	- to support lambdas in Collection classes actually
+	- pay attention to diamond problem : extending multiple classes and superclass doesn't know which method to use (same method signature). When a class implements 2 interfaces having same default method signature compiler forces to implements the default method
+	
+- static methods 
+	- only be called through the interface or class type reference, not an instance variable
+	- can not be overridden as it is for normal static methods	 
--- src/main/java/com/avivasa/workshop/java8/defaultmethod/Calculator.java
+++ src/main/java/com/avivasa/workshop/java8/defaultmethod/Calculator.java
@@ -1,48 +1,48 @@
 package com.avivasa.workshop.java8.defaultmethod;
 
-
 /**
  * default, static methods in Interface example
  * 
  * @author BAHADIR
- * */
+ */
 public interface Calculator {
 
 	/**
 	 * abstract method definition
-	 * */
-	/* public */ int sum(int a, int b); // we can omit the public modifier - all methods and constants are public by nature
-	
-	
+	 */
+	/* public */ int sum(int a, int b); // we can omit the public modifier - all
+										// methods and constants are public by
+										// nature
+
 	/**
 	 * default method implementation
-	 * */
+	 */
 	default int subtract(int a, int b) {
 		return a + b;
 	}
-	
+
 	/**
-	 * static method example 
+	 * static method example
 	 */
 	static SqrtCalculator getInstance() {
 		return new SqrtCalculator();
 	}
-	
+
 }
 
 /**
  * Calculator implementation
- * */
+ */
 class SqrtCalculator implements Calculator {
 
 	/**
 	 * @return square root of the sum
-	 * */
+	 */
 	@Override
 	public int sum(int a, int b) {
-		
+
 		return (int) Math.sqrt(a + b);
 	}
-	
+
 	// no need to implement default method
 }
